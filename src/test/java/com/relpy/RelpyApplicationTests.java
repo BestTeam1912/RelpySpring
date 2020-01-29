285bade6068619c894cd336f0370b49eeec346d3
Baldemar Sepulveda <balde.sepu@pm.me>
2020-01-28 19:38:20 -0600

    init commot

---
+++ src/test/java/com/relpy/RelpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
