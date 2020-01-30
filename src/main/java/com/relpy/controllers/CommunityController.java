3c0cc05a145ed7c7bba52a61dff12b37b7b222a2
thitalow <thitacittalow@gmail.com>
2020-01-30 13:28:14 -0600

    thita p2: very minor changes to community and mock data

--- src/main/java/com/relpy/controllers/CommunityController.java
+++ src/main/java/com/relpy/controllers/CommunityController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.relpy.models.Community;
+import com.relpy.models.Thread;
 import com.relpy.services.CommunityService;
 
 
