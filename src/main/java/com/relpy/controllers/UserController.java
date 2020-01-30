2148fc510d412510e1d23eb1c8cfe0ae92b4be82
JP204 <jp.vargas@live.com>
2020-01-29 23:31:35 -0600

    basic backend done for User

--- src/main/java/com/relpy/controllers/UserController.java
+++ src/main/java/com/relpy/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.relpy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,16 @@ public class UserController {
 	@PostMapping("/register")
 	public boolean registerUser(@RequestBody User user) {
 		return service.registerUser(user);
+	}
+	
+	@PostMapping("/login")
+	public boolean login(@RequestBody User user) {
+		return service.login(user);
+	}
+	
+	@GetMapping("/login/{id}")
+	public User getUserById(@PathVariable Long id) {
+		return service.getUserById(id);
 	}
 
 }
--- src/main/java/com/relpy/services/UserServiceImpl.java
+++ src/main/java/com/relpy/services/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.relpy.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.relpy.daos.UserDAO;
 import com.relpy.models.User;
+import com.relpy.models.UserType;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -33,6 +36,8 @@ public class UserServiceImpl implements UserService {
 		if(compUser != null) {
 			return false;
 		}else {
+			user.setDateCreated(new Date());
+			user.setType(UserType.User);
 			userRepository.save(user);
 			return true;
 		}
