f6f21005fcf79aa2f01685ddebc9e2853239a988
Cody Harkins <us3rs1@gmail.com>
2020-02-01 16:02:33 -0600

    removed some coded validation on comment service

--- src/main/java/com/relpy/services/CommentServiceImpl.java
+++ src/main/java/com/relpy/services/CommentServiceImpl.java
@@ -16,8 +16,6 @@ public class CommentServiceImpl implements CommentService {
 	@Autowired
 	private CommentDAO commentDao;
 	
-	private CommentBO commentBo = new CommentBoImpl();
-	
 	@Override
 	public List<Comment> findComments() {
 		return commentDao.findAll();
@@ -30,15 +28,7 @@ public class CommentServiceImpl implements CommentService {
 
 	@Override
 	public Comment addComment(Comment comment) throws Exception {
-		if(commentBo.validateToUpdate(comment)) {
-			throw new Exception("uh oh: this comment is already around");
-		}
-		if(commentBo.validateText(comment)) {
-			return commentDao.save(comment);
-		}else {
-			throw new Exception("Something about the text makes it invalid for posting");
-		}
-		
+		return commentDao.save(comment);	
 	}
 
 	@Override
