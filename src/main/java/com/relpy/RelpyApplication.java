f425246f2558ba8146cfbe7ee89149b52702918e
Baldemar Sepulveda <balde.sepu@pm.me>
2020-02-01 22:13:51 -0600

    changed packaging from jar to war

--- src/main/java/com/relpy/RelpyApplication.java
+++ src/main/java/com/relpy/RelpyApplication.java
@@ -1,42 +1,45 @@
 package com.relpy;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.relpy.models.Community;
-import com.relpy.models.Thread;
 import com.relpy.models.User;
 import com.relpy.services.CommunityService;
 import com.relpy.services.ThreadService;
 import com.relpy.services.UserService;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class RelpyApplication {
-	
+
 	@Value("${defaultMoney}")
 	private int defaultMoney;
-	
-	
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(RelpyApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	String helloWorld() {
+		return "Hello World!";
+	}
+
 	@Bean
-	public CommandLineRunner demoData(ThreadService threadService, CommunityService communityService, UserService userService) {
-		return args -> {					
+	public CommandLineRunner demoData(ThreadService threadService, CommunityService communityService,
+			UserService userService) {
+		return args -> {
 			User user = new User();
 			user.setUsername("balde");
 			user.setPassword("123");
 			userService.registerUser(user);
-			
+
 			user = new User();
 			user.setUsername("jp");
 			user.setPassword("1234565");
