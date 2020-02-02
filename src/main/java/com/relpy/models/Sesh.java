0ab9297ffa53be867c99e39ba8378e85518eecbb
JP204 <jp.vargas@live.com>
2020-02-01 18:54:17 -0600

    session id sent to front end on login call

---
+++ src/main/java/com/relpy/models/Sesh.java
@@ -0,0 +1,18 @@
+package com.relpy.models;
+
+public class Sesh {
+private String seshId;
+
+public Sesh() {
+	
+}
+
+public String getSeshId() {
+	return seshId;
+}
+
+public void setSeshId(String seshId) {
+	this.seshId = seshId;
+}
+
+}
