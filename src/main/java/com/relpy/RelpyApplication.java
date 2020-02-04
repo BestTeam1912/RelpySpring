885e60596cf4a81035ae643ea8564758e0102fd8
Baldemar Sepulveda <balde.sepu@pm.me>
2020-02-03 21:02:59 -0600

    fixed comp errors

--- src/main/java/com/relpy/RelpyApplication.java
+++ src/main/java/com/relpy/RelpyApplication.java
@@ -1,36 +1,14 @@
 package com.relpy;
-
-<<<<<<< HEAD
-import org.springframework.beans.factory.annotation.Value;
-=======
->>>>>>> b3
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.relpy.models.User;
-import com.relpy.services.CommunityService;
-import com.relpy.services.ThreadService;
-import com.relpy.services.UserService;
 
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
 public class RelpyApplication {
-<<<<<<< HEAD
-
-	@Value("${defaultMoney}")
-	private int defaultMoney;
-
-=======
-	
-	
-	
->>>>>>> b3
 	public static void main(String[] args) {
 		SpringApplication.run(RelpyApplication.class, args);
 	}
@@ -38,21 +16,5 @@ public class RelpyApplication {
 	@RequestMapping("/")
 	String helloWorld() {
 		return "Hello World!";
-	}
-
-	@Bean
-	public CommandLineRunner demoData(ThreadService threadService, CommunityService communityService,
-			UserService userService) {
-		return args -> {
-			User user = new User();
-			user.setUsername("balde");
-			user.setPassword("123");
-			userService.registerUser(user);
-
-			user = new User();
-			user.setUsername("jp");
-			user.setPassword("1234565");
-			userService.registerUser(user);
-		};
 	}
 }
